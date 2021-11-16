6cd462e38999e17d95fc3121a55f045550030747
Branden Palmer <brandenpalmer08@gmail.com>
2021-11-16 19:29:24 +0700

    setup dev output mode

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,9 @@ module.exports = {
       },
     ],
   },
+  mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
