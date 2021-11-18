f824560d2b0b700e8dcd766ecf4fe3272d36d190
Branden Palmer <brandenpalmer08@gmail.com>
2021-11-18 12:42:32 +0700

    add navBar container

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import "./css/style.scss";
 
-document.body.innerHTML="<div id='content'></div>";
+document.body.innerHTML = "<div id='content'></div>";
+
+// Build navBar
+(() => {
+  document.body.appendChild(buildNavBar());
+
+  function buildNavBar() {
+    const navBar = document.createElement("nav");
+    navBar.classList = "flex nav-bar";
+    navBar.appendChild(buildListOfNavItems());
+    return navBar;
+  }
+
+  function buildListOfNavItems(){
+      const list=document.createElement('ul')
+      list.appendChild
+  }
+})();
