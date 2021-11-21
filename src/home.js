fbec4e4f8a40e3f671499461c51ea817af7b271d
Branden Palmer <brandenpalmer08@gmail.com>
2021-11-21 15:50:40 +0700

    add hour info builder

--- src/home.js
+++ src/home.js
@@ -1,7 +1,7 @@
 const attachToContainer = function (container) {
   container.appendChild(buildTitle());
   container.appendChild(buildWelcomeMsg());
-  // container.appendChild(buildHourInfo());
+  container.appendChild(buildHourInfo());
   // container.appendChild(buildLocationInfo());
 };
 
@@ -21,6 +21,29 @@ function buildWelcomeMsg() {
     "Adipisicing consectetur cillum sit magna do velit ut fugiat nisi. Laborum excepteur ullamco ex in ipsum quis consequat amet incididunt anim. Ex exercitation laborum cillum qui dolor excepteur dolor consectetur. Nulla proident ut dolore non irure eu dolore aliqua consectetur laboris nisi cillum in.";
   container.appendChild(message);
   return container;
+}
+
+function buildHourInfo() {
+  const container = document.createElement("div");
+  container.classList = "hour";
+  const list = document.createElement("ul");
+  list.classList = "flex-col hour__list";
+  list.appendChild(buildHourItemWithContent("Monday"));
+  list.appendChild(buildHourItemWithContent("Tuesday"));
+  list.appendChild(buildHourItemWithContent("Wednesday"));
+  list.appendChild(buildHourItemWithContent("Thursday"));
+  list.appendChild(buildHourItemWithContent("Friday"));
+  list.appendChild(buildHourItemWithContent("Saturday"));
+  list.appendChild(buildHourItemWithContent("Sunday"));
+  container.appendChild(list);
+  return container;
+
+  function buildHourItemWithContent(content) {
+    const span = document.createElement("li");
+    span.classList = "hour__list-item";
+    span.textContent = content;
+    return span;
+  }
 }
 
 export { attachToContainer };
