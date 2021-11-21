0727cb945ac922a0684c1f97450b32b9c94e49dd
Branden Palmer <brandenpalmer08@gmail.com>
2021-11-21 15:17:28 +0700

    add welcome msg builder

--- src/home.js
+++ src/home.js
@@ -1,6 +1,6 @@
 const attachToContainer = function (container) {
   container.appendChild(buildTitle());
-  // container.appendChild(buildWelcomeMsg());
+  container.appendChild(buildWelcomeMsg());
   // container.appendChild(buildHourInfo());
   // container.appendChild(buildLocationInfo());
 };
@@ -10,6 +10,17 @@ function buildTitle() {
   title.classList = "home__title";
   title.textContent = "Home Title";
   return title;
+}
+
+function buildWelcomeMsg() {
+  const container = document.createElement("div");
+  container.classList = "flex-col home__welcome-container";
+  const message = document.createElement("p");
+  message.classList = "home__welcome-msg";
+  message.textContent =
+    "Adipisicing consectetur cillum sit magna do velit ut fugiat nisi. Laborum excepteur ullamco ex in ipsum quis consequat amet incididunt anim. Ex exercitation laborum cillum qui dolor excepteur dolor consectetur. Nulla proident ut dolore non irure eu dolore aliqua consectetur laboris nisi cillum in.";
+  container.appendChild(message);
+  return container;
 }
 
 export { attachToContainer };
