88b9b6ef5f4c2f4fc4ef2529040ba9234d252bfe
Branden Palmer <brandenpalmer08@gmail.com>
2021-11-21 18:11:44 +0700

    add menu container builder

--- src/menu.js
+++ src/menu.js
@@ -1,13 +1,24 @@
 const attachToContainer = function (container) {
-  container.appendChild(buildTitle())
-  container.appendChild(buildMenuList())
+  container.appendChild(buildMenu());
 };
+
+function buildMenu() {
+  const container = document.createElement("div");
+  container.classList = "flex-col menu";
+  container.appendChild(buildTitle());
+  container.appendChild(buildMenuList());
+  return container;
+}
 
 function buildTitle() {
   const title = document.createElement("h1");
-  title.classList = "Home__title Menu__title";
+  title.classList = "home__title menu__title";
   title.textContent = "Menu";
   return title;
+}
+
+function buildMenuList() {
+  s
 }
 
 export { attachToContainer };
