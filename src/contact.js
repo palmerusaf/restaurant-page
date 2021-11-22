3e2d984ddc47f7990f1e85aefb01ea7124fc6839
Branden Palmer <brandenpalmer08@gmail.com>
2021-11-22 13:05:31 +0700

    add title builder

--- src/contact.js
+++ src/contact.js
@@ -7,5 +7,12 @@ function buildContactInfo() {
   container.appendChild(buildTitle());
   container.appendChild(buildContactField());
   return container;
+
+  function buildTitle() {
+    const title = document.createElement("h1");
+    title.classList = "contact__title";
+    title.textContent = "Contact Us";
+    return title;
+  }
 }
 export { attachToContainer };
