c56a8a530779558eff3d0971ae21c08533c84a13
Branden Palmer <brandenpalmer08@gmail.com>
2021-11-22 13:04:55 +0700

    add contact info builder

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,11 @@
 const attachToContainer = function (container) {
-  container.textContent = "contact info";
+  container.appendChild(buildContactInfo());
 };
+function buildContactInfo() {
+  const container = document.createElement("div");
+  container.classList = "flex-col contact";
+  container.appendChild(buildTitle());
+  container.appendChild(buildContactField());
+  return container;
+}
 export { attachToContainer };
